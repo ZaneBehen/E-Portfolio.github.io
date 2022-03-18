a95a645caf3ba915230b2967a6b5b443fd6467f1
ZaneBehen <Behenzane@gmail.com>
2022-03-18 21:21:37 +1000

    'ui'

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly on Behenzane@gmail.com"
       );
     });
 }
@@ -56,3 +56,5 @@ function toggleModal() {
   isModalOpen = true;
   document.body.classList += " modal--open";
 }
+
+
